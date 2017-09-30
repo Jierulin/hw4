be25cd8c8b63ceac6b6ec72d27ada13501177abd
Jierulin <31756333+Jierulin@users.noreply.github.com>
2017-09-30 13:59:30 +0800

    Update sketch-9.js

--- sketch-9.js
+++ sketch-9.js
@@ -1,3 +1,4 @@
+background(255)
 var rate = 0.9;
 var x=0, y=0
 var targetx=mouseX, targety=mouseY;
@@ -6,6 +7,10 @@ while(true){
   fill(255,0,0)
   stroke(255,0,0)
   ellipse(x, y, 15, 15);
+	fill(255)
+	stroke(255)
+  rectMode(CENTER)
+	rect(x,y,30,30);
   x = targetx*rate + x*(1-rate); // rate is 0.1, or 10% -- (1-rate) is 0.9, or 90%
 	y=targety*rate+y*(1-rate);
 
